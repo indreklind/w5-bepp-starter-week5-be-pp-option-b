1b729d85c784473fe2517ccfa7e0b1cae445cbc5
Indrek <indreklindsalu04@gmail.com>
2026-02-09 13:47:26 +0200

    Step 0B

---
+++ README.md
@@ -0,0 +1,7 @@
+
+<!-- 
+```sh
+npm i express mongoose morgan @google/genai dotenv
+npm i jest nodemon supertest -D
+``` 
+-->
---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+// const auth = (req, res, next) => {
+//     if (req.query.admin === 'true') {
+//         next();
+//         return
+//     }
+//     res.status(403).send('Unauthorized');
+// };
+
+// module.exports = auth;
+
