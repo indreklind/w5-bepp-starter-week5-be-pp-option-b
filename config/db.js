e92dc1b5d9f6988ee69c8f93a6312abfc5dc46a5
Indrek <indreklindsalu04@gmail.com>
2026-02-09 15:22:33 +0200

    chore: use environment variable for database URI

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
-    console.log(error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
-};
+}
  
 module.exports = connectDB;
